d13f6e999124ad53770019728b444a29b7fcce2d
gch2505 <gch2505@gmail.com>
2024-02-26 18:10:12 +0900

    포트폴리오 1.1.1

--- script/main.js
+++ script/main.js
@@ -1,5 +1,48 @@
+window.onload = function(){
+  const elm = document.querySelectorAll('.section');
+  const elmCount = elm.length;
+  elm.forEach(function(item, index){
+    item.addEventListener('mousewheel', function(event){
+      event.preventDefault();
+      let delta = 0;
+
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+          delta = event.wheelDelta / 120;
+          if (window.opera) delta = -delta;
+      } 
+      else if (event.detail)
+          delta = -event.detail / 3;
+
+      let moveTop = window.scrollY;
+      let elmSelector = elm[index];
+
+      // wheel down : move to next section
+      if (delta < 0){
+        if (elmSelector !== elmCount-1){
+          try{
+            moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
+      // wheel up : move to previous section
+      else{
+        if (elmSelector !== 0){
+          try{
+            moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
+
+      const body = document.querySelector('html');
+      window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+    });
+  });
+}
+
 
 $(document).ready(function(){
+
 
   // 인트로 1
   setTimeout(function(){
@@ -98,10 +141,12 @@ $(document).ready(function(){
     let i = $(this).index();
     console.log(i);
 
+  
     if(i>=2){
-      i = i+4;
+      i = i+5;
     }else{
       i = $(this).index();
+      i = i+1;
     }
 
     $('html, body').stop().animate({scrollTop:$('main section').eq(i).offset().top},500);
