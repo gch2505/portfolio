aaaae56f5780a4e54d45cae090cb63ee67d6be46
gch2505 <gch2505@gmail.com>
2024-02-26 17:26:02 +0900

    포트폴리오 1.1

--- script/main.js
+++ script/main.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
   setTimeout(function(){
     $('.intro2').fadeIn(1000);
     $('main').fadeIn(1000);
-    $('.sub').fadeIn(1000);
   },7000);
   setTimeout(function(){
     $('.h_wrap').fadeIn(500);
@@ -44,21 +43,21 @@ $(document).ready(function(){
 
 
 
-  //밤하늘 별
-  const size = ['size1', 'size2', 'size3'];
-  const opacity = ['opacity1', 'opacity1', 'opacity1', 'opacity2', 'opacity3'];
-  const twinkle = ['twinkle1', 'twinkle1', 'twinkle1', 'twinkle2', 'twinkle2', 'twinkle3', 'twinkle4'];
+  // //밤하늘 별
+  // const size = ['size1', 'size2', 'size3'];
+  // const opacity = ['opacity1', 'opacity1', 'opacity1', 'opacity2', 'opacity3'];
+  // const twinkle = ['twinkle1', 'twinkle1', 'twinkle1', 'twinkle2', 'twinkle2', 'twinkle3', 'twinkle4'];
 
-  //랜덤함수
-  function getRandomValue(max){
-    return Math.floor(Math.random() * max);
-  }
+  // //랜덤함수
+  // function getRandomValue(max){
+  //   return Math.floor(Math.random() * max);
+  // }
 
-  const _s = getRandomValue(3);
-  const _o = getRandomValue(5);
-  const _t = getRandomValue(7);
+  // const _s = getRandomValue(3);
+  // const _o = getRandomValue(5);
+  // const _t = getRandomValue(7);
 
-  const className = size[_s] + ' ' + opacity[_o] + ' ' + twinkle[_t];
+  // const className = size[_s] + ' ' + opacity[_o] + ' ' + twinkle[_t];
 
 
   //윈도우 높이
@@ -67,12 +66,15 @@ $(document).ready(function(){
     let sec_pos = $('section').offset().top;
     console.log(s_pos);
 
+
     if(s_pos>=sec_pos){
       $('.top_btn').fadeIn();
       $('.h_wrap').addClass('h_top');
+      $('.sub').fadeIn();
     }else{
       $('.top_btn').fadeOut();
       $('.h_wrap').removeClass('h_top');
+      $('.sub').fadeOut();
     }
 
   });
@@ -107,6 +109,18 @@ $(document).ready(function(){
     return false;
   });
 
+  //프로젝트 리스트에서 프로젝트 클릭하면 해당 페이지로 스크롤
+  let pj_list = $('.project li');
+  pj_list.click(function(){
+    let i = $(this).index();
+    console.log(i);
+
+    i = i+2;
+    $('html, body').stop().animate({scrollTop:$('main section').eq(i).offset().top},500);
+
+    return false;
+  });
+
 
 
   //snb 클릭시 snb 모양 바뀌게
@@ -136,6 +150,62 @@ $(document).ready(function(){
       'opacity':'0',
       'line-height':'30px'
     });
+  });
+
+
+  //갤러리 스크립트
+  const g_menu = $('.g_menu li a');
+  const g_img = $('.g_list a');
+
+  const all_btn = $('.g_menu .g_all');
+  const design_btn = $('.g_menu .g_uiux');
+  const uiux_btn = $('.g_menu .g_design');
+  const art_btn = $('.g_menu .g_art');
+
+  g_menu.click(function(){
+    $(this).addClass('action').parent().siblings().find('a').removeClass('action');
+  });
+
+  //버튼 클릭시 해당 태그만 보이도록
+  all_btn.click(function(){
+    $('.g_photo').hide();
+    $('.g_photo').fadeIn();
+  });
+  design_btn.click(function(){
+    $('.g_photo').hide();
+    $('.design').fadeIn();
+  });
+  uiux_btn.click(function(){
+    $('.g_photo').hide();
+    $('.uiux').fadeIn();
+  });
+  art_btn.click(function(){
+    $('.g_photo').hide();
+    $('.art').fadeIn();
+  });
+  
+  //갤러리 클릭시 모달창 띄우기
+  g_img.click(function(){
+    let img_src = $(this).attr('href');
+    console.log(img_src);
+
+    let modal = `
+      <div class="modal">
+        <div class="center">
+          <img src="${img_src}" alt="">
+          <i class="fas fa-times"></i>
+        </div>
+      </div>
+    `;
+
+    $('body').append(modal);
+    const close_btn = $('.modal .center .fas');
+
+    close_btn.click(function(){
+      $('.modal').fadeOut(250);
+    });
+
+    return false;
   });
 
 });
